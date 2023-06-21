d23ea5513e8e587045c80b48741acdc272a7880e
juanfajardo <juanfajardo@paradigma.com.co>
2023-06-20 20:34:55 -0500

    Integracion delproyecto

---
+++ PlatziWebAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatziWebAzure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
